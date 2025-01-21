338681dd6d257e7fb7337f7a0cfe91d43b4d1a73
YatFungLoo <looyatfung@gmail.com>
2025-01-21 23:05:48 +0800

    Added data set

--- include/header.hpp
+++ include/header.hpp
@@ -1,6 +1,11 @@
+#ifndef HEADER_H
+#define HEADER_H
+
 #include <iostream>
 
 class Example {
   public:
     void Print();
 };
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,20 @@
 #include "header.hpp"
-#include <cassert>
+#include "data.hpp"
 
 int main() {
+    std::vector<Rod> data{
+	{1, 1},
+	{2, 5},
+	{3, 8},
+	{4, 9},
+	{5, 10},
+	{6, 17},
+	{7, 17},
+	{8, 20},
+	{9, 24},
+	{10, 30},
+    };
+
     Example myExample;
     myExample.Print();
 }

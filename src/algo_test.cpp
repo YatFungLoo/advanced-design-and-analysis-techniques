58b07c26265cfcf856e9e87b8a9cabcc3df63030
YatFungLoo <looyatfung@gmail.com>
2025-01-22 17:44:32 +0800

    move to gtest, from testing with main

--- src/algo_test.cpp
+++ src/algo_test.cpp
@@ -1,9 +1,24 @@
+#include "algo.hpp"
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
-  // Expect equality.
-  EXPECT_EQ(7 * 6, 42);
+TEST(cutRodTest, BasicAssertions) {
+    const std::array<Rod, NUM_LENGTH> data{{
+        {0, 0},
+        {1, 1},
+        {2, 5},
+        {3, 8},
+        {4, 9},
+        {5, 10},
+        {6, 17},
+        {7, 17},
+        {8, 20},
+        {9, 24},
+        {10, 30},
+    }};
+    auto i = Algo::cutRod(0, data);
+    // Expect equality.
+    EXPECT_EQ(0, i);
 }

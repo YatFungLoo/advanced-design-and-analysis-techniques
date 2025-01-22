f0a791f8deabb92e4d6491417d0c40f16053dac8
YatFungLoo <looyatfung@gmail.com>
2025-01-22 13:55:44 +0800

    added untested rodCut function.

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,36 @@
-#include "header.hpp"
 #include "data.hpp"
+#include "header.hpp"
+#include <array>
+#include <algorithm>
+
+[[maybe_unused]] int cutRod(int length, std::array<Rod, NUM_LENGTH> const &data);
 
 int main() {
-    std::vector<Rod> data{
-	{1, 1},
-	{2, 5},
-	{3, 8},
-	{4, 9},
-	{5, 10},
-	{6, 17},
-	{7, 17},
-	{8, 20},
-	{9, 24},
-	{10, 30},
-    };
+    const std::array<Rod, NUM_LENGTH> data{{
+	{0, 0},
+        {1, 1},
+        {2, 5},
+        {3, 8},
+        {4, 9},
+        {5, 10},
+        {6, 17},
+        {7, 17},
+        {8, 20},
+        {9, 24},
+        {10, 30},
+    }};
+}
 
-    Example myExample;
-    myExample.Print();
+// @breif Top down approach.
+[[maybe_unused]] int cutRod(int length, std::array<Rod, NUM_LENGTH> const &data) {
+    if (length == 0) {
+        return 0;
+    }
+
+    int price = 0;
+    for (int i = 0; i < length; i++) {
+	price = std::max(price, (data[i].price + cutRod(length - i, data)));
+    }
+
+    return price;
 }

5f428ecda9b8f13a14f9137df7ec3451f6f9f337
YatFungLoo <looyatfung@gmail.com>
2025-02-23 22:23:08 +0800

    Re: CutRod top down approach + gtest
    Commit fixup from Jan23, 2025

--- src/algo.cpp
+++ src/algo.cpp
@@ -1,19 +1,17 @@
 #include "algo.hpp"
 
-void Example::Print() {
-    std::cout << "Hello World." << std::endl;
-}
+void Example::Print() { std::cout << "Hello World." << std::endl; }
 
-// @breif Top down approach.
+// @brief Recursive top-down implementation, return maximum price of rod input length.
 [[maybe_unused]] int Algo::cutRod(int length, std::array<Rod, NUM_LENGTH> const &data) {
     if (length == 0) {
         return 0;
     }
 
     int price = 0;
-    for (int i = 0; i < length; i++) {
-	price = std::max(price, (data[i].price + cutRod(length - i, data)));
+    for (int i = 1; i <= length; i++) {
+        price = std::max(price, (data[i].price + cutRod(length - i, data)));
     }
 
     return price;
-};
+}
--- src/algo_test.cpp
+++ src/algo_test.cpp
@@ -1,24 +1,26 @@
 #include "algo.hpp"
 #include <gtest/gtest.h>
 
+// Price table.
+static const std::array<Rod, NUM_LENGTH> data{{
+    {0, 0},
+    {1, 1},
+    {2, 5},
+    {3, 8},
+    {4, 9},
+    {5, 10},
+    {6, 17},
+    {7, 17},
+    {8, 20},
+    {9, 24},
+    {10, 30},
+}};
+// Answers.
+static const std::vector<int> ans{0, 1, 5, 8, 10, 13, 17, 18, 22, 25, 30};
+
 // Demonstrate some basic assertions.
-  // Expect two strings not to be equal.
-  EXPECT_STRNE("hello", "world");
-TEST(cutRodTest, BasicAssertions) {
-    const std::array<Rod, NUM_LENGTH> data{{
-        {0, 0},
-        {1, 1},
-        {2, 5},
-        {3, 8},
-        {4, 9},
-        {5, 10},
-        {6, 17},
-        {7, 17},
-        {8, 20},
-        {9, 24},
-        {10, 30},
-    }};
-    auto i = Algo::cutRod(0, data);
-    // Expect equality.
-    EXPECT_EQ(0, i);
-}
+TEST(cutRodTest, HandlesPositiveInput) {
+    for (int i = 0; i < NUM_LENGTH; i++) {
+        EXPECT_EQ(Algo::cutRod(i, data), ans[i]) << "i is : " << i << std::endl;;
+    }
+};
